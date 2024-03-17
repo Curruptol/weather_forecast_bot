55a219ec41e762fb8a7da13f46a1ac441e13f02a
nikitasokolov <enteerk@yandex.ru>
2024-03-17 03:35:35 +0300

    bot_weather_forecast_test

--- main.py
+++ main.py
@@ -18,19 +18,17 @@ async def start_bot():
 
 @dp.message(F.text == "/start")
 async def start(message: Message):
-    await message.answer(f"Привет, {message.from_user.last_name} {message.from_user.first_name}!")
+    await message.answer(f"Привет, {message.from_user.first_name}!")
     await message.answer(f"Напиши название города")
 
+@dp.message()
 async def send_weather(message: Message):
-    pass
-
-def get_weather(city, open_weather_api_key: config.OPEN_WEATHER_API_KEY):
     try:
         req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?"
-                           f"q={city}"
+                           f"q={message.text}"
                            f"&lang=ru"
                            f"&units=metric"
-                           f"&appid={open_weather_api_key}"
+                           f"&appid={config.OPEN_WEATHER_API_KEY}"
                            )
         request_data = req.json()
         # pprint(request_data)
@@ -48,23 +46,62 @@ def get_weather(city, open_weather_api_key: config.OPEN_WEATHER_API_KEY):
         cur_sunset = dt.time(cur_timestamp_sunset)
         cur_day_len = cur_timestamp_sunset - cur_timestamp_sunrise
 
-        print(f"Погода в городе {cur_city}\n"
-              f"Текущая температура: {cur_temp}, ощущается как {cur_feels_like}\n"
-              f"Макс. температура: {cur_temp_max}. Мин. температура: {cur_temp_min}\n"
-              f"Текущая влажность: {cur_humidity}\n"
-              f"Текущее давление {cur_pressure}\n"
-              f"Восход в {cur_sunrise}\n"
-              f"Закат в {cur_sunset}\n"
-              f"Продолжительность дня: {cur_day_len}"
-             )
+        await message.answer(f"Погода в городе {cur_city}\n"
+                             f"Текущая температура: {cur_temp}, ощущается как {cur_feels_like}\n"
+                             f"Макс. температура: {cur_temp_max}. Мин. температура: {cur_temp_min}\n"
+                             f"Текущая влажность: {cur_humidity}\n"
+                             f"Текущее давление {cur_pressure}\n"
+                             f"Восход в {cur_sunrise}\n"
+                             f"Закат в {cur_sunset}\n"
+                             f"Продолжительность дня: {cur_day_len}"
+                            )
 
     except Exception as exc:
         print(exc)
         print("Неверное название города")
 
+# def get_weather(city, open_weather_api_key: config.OPEN_WEATHER_API_KEY):
+#     try:
+#         req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?"
+#                            f"q={city}"
+#                            f"&lang=ru"
+#                            f"&units=metric"
+#                            f"&appid={open_weather_api_key}"
+#                            )
+#         request_data = req.json()
+#         # pprint(request_data)
+#
+#         cur_city = request_data["name"]
+#         cur_temp = math.ceil(request_data["main"]["temp"])
+#         cur_humidity = request_data["main"]["humidity"]
+#         cur_pressure = request_data["main"]["pressure"]
+#         cur_feels_like = math.ceil(request_data["main"]["feels_like"])
+#         cur_temp_max = math.ceil(request_data["main"]["temp_max"])
+#         cur_temp_min = math.ceil(request_data["main"]["temp_min"])
+#         cur_timestamp_sunrise = dt.fromtimestamp(request_data["sys"]["sunrise"])
+#         cur_timestamp_sunset = dt.fromtimestamp(request_data["sys"]["sunset"])
+#         cur_sunrise = dt.time(cur_timestamp_sunrise)
+#         cur_sunset = dt.time(cur_timestamp_sunset)
+#         cur_day_len = cur_timestamp_sunset - cur_timestamp_sunrise
+#
+#         print(f"Погода в городе {cur_city}\n"
+#               f"Текущая температура: {cur_temp}, ощущается как {cur_feels_like}\n"
+#               f"Макс. температура: {cur_temp_max}. Мин. температура: {cur_temp_min}\n"
+#               f"Текущая влажность: {cur_humidity}\n"
+#               f"Текущее давление {cur_pressure}\n"
+#               f"Восход в {cur_sunrise}\n"
+#               f"Закат в {cur_sunset}\n"
+#               f"Продолжительность дня: {cur_day_len}"
+#              )
+#
+#     except Exception as exc:
+#         print(exc)
+#         print("Неверное название города")
+
 def main():
-    city = input("Введите город:\n")
-    get_weather(city, config.OPEN_WEATHER_API_KEY)
+    pass
+    # city = input("Введите город:\n")
+    # get_weather(city, config.OPEN_WEATHER_API_KEY)
 
 if __name__ == '__main__':
     asyncio.run(start_bot())
