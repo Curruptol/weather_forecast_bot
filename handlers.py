c3af313d0d4ead7187f22541e60a2db7f78073c9
nikitasokolov <enteerk@yandex.ru>
2024-03-22 01:27:39 +0300

    telegram bot

--- handlers.py
+++ handlers.py
@@ -34,30 +34,33 @@ async def input_city(callback: CallbackQuery, state: FSMContext):
 
 @router.message(Periods.weather_period)
 @router.callback_query(F.data == "back_to_menu")
-async def back_from_weather_period(callback: CallbackQuery):
+async def back_from_weather_period(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Periods.menu)
     await callback.message.edit_text(f"📍Главное меню", reply_markup=kb.menu)
 
 
 @router.message(Periods.current_day)
 @flags.chat_action("typing")
-async def send_weather_today(msg: Message):
+async def send_weather_today(msg: Message, state: FSMContext):
     ans = weather.get_weather(msg.text)
     msg_wait = await msg.answer(f"⏳Пожалуйста, подожди немного, сейчас я отправлю тебе погоду...")
     await asyncio.sleep(1.5)
     if ans is None:
+        await state.set_state(Periods.incorrect_city)
         await msg_wait.edit_text(f"❗️Неверный город❗️", reply_markup=kb.try_again)
     else:
+        await state.set_state(Periods.weather_sent)
         await msg_wait.edit_text(ans, reply_markup=kb.exit_to_menu)
 
 
-@router.message(Periods.current_day)
+@router.message(Periods.weather_sent)
 @router.callback_query(F.data == "menu")
 async def back_to_menu(callback: CallbackQuery, state: FSMContext):
     await state.set_state(Periods.menu)
     await callback.message.answer(f"📍Главное меню", reply_markup=kb.menu)
 
 
-@router.message(Periods.current_day)
+@router.message(Periods.incorrect_city)
 @router.callback_query(F.data == "try_again")
 async def try_again(callback: CallbackQuery, state: FSMContext):
     await input_city(callback, state)
--- states.py
+++ states.py
@@ -5,3 +5,5 @@ class Periods(StatesGroup):
     menu = State()
     weather_period = State()
     current_day = State()
+    incorrect_city = State()
+    weather_sent = State()
