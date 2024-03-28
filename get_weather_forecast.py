6f87807ddca83851ccceca1216d6e9399b2dc6be
nikitasokolov <enteerk@yandex.ru>
2024-03-28 16:37:22 +0300

    telegram bot

--- get_weather_forecast.py
+++ get_weather_forecast.py
@@ -22,27 +22,47 @@ def get_weather_forecast(city):
         weather_forecast = WeatherForecastData(**request_data)
 
         city = weather_forecast.get("location").get("name")
+
         date = weather_forecast.get("forecast").get("forecastday")[1].get("date")
         date = dt.strptime(date, "%Y-%m-%d").strftime('%d.%m.%Y')
-        max_temp = math.ceil(weather_forecast.get("forecast").get("forecastday")[1].get("day").get("maxtemp_c"))
-        min_temp = math.ceil(weather_forecast.get("forecast").get("forecastday")[1].get("day").get("mintemp_c"))
-        avg_temp = math.ceil(weather_forecast.get("forecast").get("forecastday")[1].get("day").get("avgtemp_c"))
-        max_wind_speed = (weather_forecast.get("forecast").get("forecastday")[1].get("day").get("maxwind_kph")) / 3.6
+
+        max_temp = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("maxtemp_c")
+        max_temp = math.ceil(max_temp)
+
+        min_temp = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("mintemp_c")
+        min_temp = math.ceil(min_temp)
+
+        avg_temp = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("avgtemp_c")
+        avg_temp = math.ceil(avg_temp)
+
+        max_wind_speed = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("maxwind_kph")
+        max_wind_speed = max_wind_speed / 3.6
         max_wind_speed = round(max_wind_speed, 1)
+
         precipitation = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("totalprecip_mm")
+
         avg_humidity = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("avghumidity")
+
         chance_of_rain = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("daily_chance_of_rain")
+
         weather_condition = weather_forecast.get("forecast").get("forecastday")[1].get("day").get("condition").get("text")
+
         sunrise = weather_forecast.get("forecast").get("forecastday")[1].get("astro").get("sunrise")
         sunrise = dt.strptime(sunrise, "%I:%M %p")
         sunrise = dt.strftime(sunrise, "%H:%M")
+
         sunset = weather_forecast.get("forecast").get("forecastday")[1].get("astro").get("sunset")
         sunset = dt.strptime(sunset, "%I:%M %p")
         sunset = dt.strftime(sunset, "%H:%M")
-        pressure = (weather_forecast.get("forecast").get("forecastday")[1].get("hour")[0].get("pressure_mb")) / 1.333
+
+        pressure = weather_forecast.get("forecast").get("forecastday")[1].get("hour")[0].get("pressure_mb")
+        pressure = pressure / 1.333
         pressure = round(pressure)
+
         cloud = weather_forecast.get("forecast").get("forecastday")[1].get("hour")[0].get("cloud")
-        gust = (weather_forecast.get("forecast").get("forecastday")[1].get("hour")[0].get("gust_kph")) / 3.6
+
+        gust = weather_forecast.get("forecast").get("forecastday")[1].get("hour")[0].get("gust_kph")
+        gust = gust / 3.6
         gust = round(gust, 1)
 
         total_weather_forecast = (f"☀️Погода на завтра {date} в городе {city}: {weather_condition}\n\n"
